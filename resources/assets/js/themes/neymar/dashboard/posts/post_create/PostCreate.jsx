43cfa986c3bb3950589e420fc83fcc0c23a02f0c
= <ypnwebdev@gmail.com>
2018-08-29 22:43:22 +0700

    ok

--- resources/assets/js/themes/neymar/dashboard/posts/post_create/PostCreate.jsx
+++ resources/assets/js/themes/neymar/dashboard/posts/post_create/PostCreate.jsx
@@ -14,44 +14,6 @@ import NavigationIcon from '@material-ui/icons/Navigation';
 import AvatarEditor from 'react-avatar-editor'
 
 
-import ReactCrop from 'react-image-crop';
-import 'react-image-crop/dist/ReactCrop.css';
-
-function getCroppedImg(image, pixelCrop, fileName) {
-
-  //alert('#cropt');
-
-  const canvas = document.createElement('canvas');
-  canvas.width = pixelCrop.width;
-  canvas.height = pixelCrop.height;
-  const ctx = canvas.getContext('2d');
-
-  ctx.drawImage(
-    image,
-    pixelCrop.x,
-    pixelCrop.y,
-    pixelCrop.width,
-    pixelCrop.height,
-    0,
-    0,
-    pixelCrop.width,
-    pixelCrop.height
-  );
-
-  // As Base64 string
-  const base64Image = canvas.toDataURL('image/jpeg');
-
-  // As a blob
-  // return new Promise((resolve, reject) => {
-  //   canvas.toBlob(file => {
-  //     file.name = fileName;
-  //     resolve(file);
-  //   }, 'image/jpeg');
-  // });
-
-  return base64Image;
-}
-
 
 export default class PostCreate extends React.Component{
 
@@ -59,13 +21,6 @@ export default class PostCreate extends React.Component{
       super();
       this.state = {
         image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8--W3In1em5F5MTwuDGCu6wPa_iYls2T139ymHxNKF01VFot4RA',
-        crop: {
-           x: 20,
-           y: 10,
-           width:200,
-           height:400,
-           aspect: 16/9
-        }
       }
     }
 
@@ -81,47 +36,36 @@ export default class PostCreate extends React.Component{
       var input = event.target;
       var _self = this;
 
+
+      var image, file;
+
+
       if (input.files && input.files[0]) {
 
-         var reader = new FileReader();
+        image = new Image();
 
-         reader.onload = function(e) {
-            //$('#blah').attr('src', e.target.result);
-           _self.setState({
-              image:e.target.result
-           })
-         }
+         image.onload = function() {
 
-         reader.readAsDataURL(input.files[0]);
+             alert("The image width is " +input.width + " and image height is " + input.height);
+         };
        }
 
     }
 
-    onImageLoaded(){
-      //alert('image loaded');
+    saveImage(){
+      const canvas = this.editor.getImage().toDataURL();
+      let imageURL;
+      fetch(canvas)
+        .then(res => res.blob())
+        .then(blob => (imageURL = window.URL.createObjectURL(blob)));
+        console.log(canvas);
+
+        this.setState({
+          image:canvas
+        })
     }
 
-    onComplete(){
-      alert('image croped loaded');
-    }
-
-    cropChange(crop){
-      this.setState({ crop });
-    }
-
-    async cropImage(){
-
-      const image =  $('.ReactCrop__image')[0];
-      const pixelCrop = this.state.crop;
-      const fileName = 'result_crop';
-
-      const croppedImg = await getCroppedImg(image,pixelCrop,fileName);
-
-      this.setState({
-        image:croppedImg
-      })
-
-    }
+    setEditorRef(editor){this.editor = editor}
 
     render(){
       return(
@@ -149,16 +93,16 @@ export default class PostCreate extends React.Component{
                   <br/><br/>
                   <div>
                     <AvatarEditor
+                      ref={this.setEditorRef.bind(this)}
                       image={this.state.image}
                       width={345}
                       height={194}
-                      border={50}
-                      color={[255, 255, 255, 0.6]} // RGBA
-                      scale={1.2}
+                      border={0}
+                      color={[255, 255, 255, 0.6]}
+                      scale={4}
                       rotate={0}
                     />
-
-                    <button onClick={this.cropImage.bind(this)}>crop</button>
+                  <button onClick={this.saveImage.bind(this)}>Save Image</button>
                   </div>
                 <br/>
                 <br/>
