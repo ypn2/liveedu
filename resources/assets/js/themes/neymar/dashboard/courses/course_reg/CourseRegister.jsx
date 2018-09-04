fbef3f2b4d206bea458f7593c9999a76564f8d6a
= <ypnwebdev@gmail.com>
2018-09-04 22:48:44 +0700

    hptt

--- resources/assets/js/themes/neymar/dashboard/courses/course_reg/CourseRegister.jsx
+++ resources/assets/js/themes/neymar/dashboard/courses/course_reg/CourseRegister.jsx
@@ -16,12 +16,160 @@ import ListItemText from '@material-ui/core/ListItemText';
 import Select from '@material-ui/core/Select';
 import Checkbox from '@material-ui/core/Checkbox';
 import Chip from '@material-ui/core/Chip';
+import InputAdornment from '@material-ui/core/InputAdornment';
 
 /*import component*/
 import ThemeContext from '../../../configs/context';
 import CourseCurriculum from './components/CourseCurriculum';
 
 import axios from 'axios';
+
+var ChuSo=new Array(" không "," một "," hai "," ba "," bốn "," năm "," sáu "," bảy "," tám "," chín ");
+var Tien=new Array( "", " ngàn", " triệu", " tỷ", " ngàn tỷ", " triệu tỷ");
+
+//1. Hàm đọc số có ba chữ số;
+function DocSo3ChuSo(baso)
+{
+    var tram;
+    var chuc;
+    var donvi;
+    var KetQua="";
+    tram=parseInt(baso/100);
+    chuc=parseInt((baso%100)/10);
+    donvi=baso%10;
+    if(tram==0 && chuc==0 && donvi==0) return "";
+    if(tram!=0)
+    {
+        KetQua += ChuSo[tram] + " trăm ";
+        if ((chuc == 0) && (donvi != 0)) KetQua += " linh ";
+    }
+    if ((chuc != 0) && (chuc != 1))
+    {
+            KetQua += ChuSo[chuc] + " mươi";
+            if ((chuc == 0) && (donvi != 0)) KetQua = KetQua + " linh ";
+    }
+    if (chuc == 1) KetQua += " mười ";
+    switch (donvi)
+    {
+        case 1:
+            if ((chuc != 0) && (chuc != 1))
+            {
+                KetQua += " mốt ";
+            }
+            else
+            {
+                KetQua += ChuSo[donvi];
+            }
+            break;
+        case 5:
+            if (chuc == 0)
+            {
+                KetQua += ChuSo[donvi];
+            }
+            else
+            {
+                KetQua += " lăm ";
+            }
+            break;
+        default:
+            if (donvi != 0)
+            {
+                KetQua += ChuSo[donvi];
+            }
+            break;
+        }
+    return KetQua;
+}
+
+//2. Hàm đọc số thành chữ (Sử dụng hàm đọc số có ba chữ số)
+
+function DocTienBangChu(SoTien)
+{
+    var lan=0;
+    var i=0;
+    var so=0;
+    var KetQua="";
+    var tmp="";
+    var ViTri = new Array();
+    if(SoTien<0) return "Số tiền âm !";
+    if(SoTien==0) return "Không đồng !";
+    if(SoTien>0)
+    {
+        so=SoTien;
+    }
+    else
+    {
+        so = -SoTien;
+    }
+    if (SoTien > 100000000)
+    {
+        //SoTien = 0;
+        return "Số quá lớn!";
+    }
+    ViTri[5] = Math.floor(so / 1000000000000000);
+    if(isNaN(ViTri[5]))
+        ViTri[5] = "0";
+    so = so - parseFloat(ViTri[5].toString()) * 1000000000000000;
+    ViTri[4] = Math.floor(so / 1000000000000);
+     if(isNaN(ViTri[4]))
+        ViTri[4] = "0";
+    so = so - parseFloat(ViTri[4].toString()) * 1000000000000;
+    ViTri[3] = Math.floor(so / 1000000000);
+     if(isNaN(ViTri[3]))
+        ViTri[3] = "0";
+    so = so - parseFloat(ViTri[3].toString()) * 1000000000;
+    ViTri[2] = parseInt(so / 1000000);
+     if(isNaN(ViTri[2]))
+        ViTri[2] = "0";
+    ViTri[1] = parseInt((so % 1000000) / 1000);
+     if(isNaN(ViTri[1]))
+        ViTri[1] = "0";
+    ViTri[0] = parseInt(so % 1000);
+  if(isNaN(ViTri[0]))
+        ViTri[0] = "0";
+    if (ViTri[5] > 0)
+    {
+        lan = 5;
+    }
+    else if (ViTri[4] > 0)
+    {
+        lan = 4;
+    }
+    else if (ViTri[3] > 0)
+    {
+        lan = 3;
+    }
+    else if (ViTri[2] > 0)
+    {
+        lan = 2;
+    }
+    else if (ViTri[1] > 0)
+    {
+        lan = 1;
+    }
+    else
+    {
+        lan = 0;
+    }
+    for (i = lan; i >= 0; i--)
+    {
+       tmp = DocSo3ChuSo(ViTri[i]);
+       KetQua += tmp;
+       if (ViTri[i] > 0) KetQua += Tien[i];
+       if ((i > 0) && (tmp.length > 0)) KetQua += ',';//&& (!string.IsNullOrEmpty(tmp))
+    }
+   if (KetQua.substring(KetQua.length - 1) == ',')
+   {
+        KetQua = KetQua.substring(0, KetQua.length - 1);
+   }
+   KetQua = KetQua.substring(1,2).toUpperCase()+ KetQua.substring(2);
+   return KetQua;//.substring(0, 1);//.toUpperCase();// + KetQua.substring(1);
+}
+
+const numberWithCommas = (x) => {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 
 const styles = {
   card: {
@@ -79,6 +227,10 @@ class CourseRegister extends React.Component{
 
     this.state = {
       name: [],
+      course_price_number:0,
+      course_price_text:'Không',
+      course_lesson_number:0,
+      course_lesson_text:'Không'
     };
 
   }
@@ -107,6 +259,24 @@ class CourseRegister extends React.Component{
     });
   }
 
+  onValueChange(event){
+    const amount = event.target.value;
+
+    this.setState({
+      course_price_text:DocTienBangChu(amount),
+      course_price_number:numberWithCommas(amount)
+    })
+  }
+
+  onLessonPriceChange(event){
+
+    const amount = event.target.value;
+    this.setState({
+      course_lesson_text:DocTienBangChu(amount),
+      course_lesson_number:numberWithCommas(amount)
+    })
+  }
+
 
   render(){
 
@@ -130,21 +300,38 @@ class CourseRegister extends React.Component{
                 margin="normal"
                 className={classes.textfield}
               />
+              <div>
               <TextField
                 required
                 id="course_price"
                 label="Giá khóa học"
+                type="number"
                 margin="normal"
-                className={classes.textfield}
-              />
+                InputProps={{
+                  endAdornment: <InputAdornment position="start">Đồng</InputAdornment>,
+                }}
+                style={{width:300}}
+                onChange = {this.onValueChange.bind(this)}
+              />		&nbsp;	&nbsp;	&nbsp;
 
+              <label>Định dạng: </label>&nbsp;<span style={{color:'red',fontWeight:'bold'}}>{this.state.course_price_number}</span>&nbsp;đồng | <label>Bằng chữ:</label>&nbsp;<span style={{color:'green',fontWeight:'bold'}}>{this.state.course_price_text}</span>&nbsp;đồng
+              </div>
+              <div>
               <TextField
                 required
+                type="number"
                 id="lesson_price"
                 label="Giá bài từng buổi học"
+                onChange={this.onLessonPriceChange.bind(this)}
                 margin="normal"
-                className={classes.textfield}
-              />
+                InputProps={{
+                  endAdornment: <InputAdornment position="start">Đồng</InputAdornment>,
+                }}
+                style={{width:300}}
+              />	&nbsp;	&nbsp;	&nbsp;
+
+              <label>Định dạng: </label>&nbsp;<span style={{color:'red',fontWeight:'bold'}}>{this.state.course_lesson_number}</span>&nbsp;đồng | <label>Bằng chữ:</label>&nbsp;<span style={{color:'green',fontWeight:'bold'}}>{this.state.course_lesson_text}</span>&nbsp;đồng
+              </div>
 
               <FormControl className={classes.textfield}>
                 <InputLabel htmlFor="select-multiple-checkbox">{context.txt_input_control.course_reg.course_entry_field}</InputLabel>
